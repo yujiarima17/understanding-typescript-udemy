568a5b87bc2f6732b2d188885b5b221dcf0ce6f9
Yuji Arima <arimayuji2@gmail.com>
2023-11-19 12:57:50 -0300

    second section - (10-15)

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 5;
+var printResult = true;
+var resultPhrase = 'Result is:';
+add(number1, number2, printResult, resultPhrase);
